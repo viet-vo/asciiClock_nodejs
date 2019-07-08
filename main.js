a18751714ab5b86c5ab51d03abe0a46b94ef270e
Viet Vo <viet.allen.qvo@gmail.com>
2019-07-07 23:28:25 -0700

    built function that take in number" and concats specific patterns

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const hour = hour1 + hour2;
 const minute1 = time.charAt(3);
 const minute2 = time.charAt(4);
 const minute = minute1 + minute2;
-let isMorning;
+let isMorning,
+    topLine,
+    midLine,
+    botLine;
 
 if (hour <= 12) {
     dayHalf = true;
@@ -14,7 +17,7 @@ if (hour <= 12) {
 };
 
 console.log(
-`
+    `
  _   _     _   _
 |_| |_| * |_| |_|
 |_| |_| * |_| |_|
@@ -34,3 +37,58 @@ console.log(
 //   midLine.concat('|_|');
 //   botLine.concat('  |');
 // };
+
+function numCreate(number) {
+    switch (number) {
+        case "0":
+            topLine.concat(' _ ');
+            midLine.concat('| |');
+            botLine.concat('|_|');
+            break;
+        case "1":
+            topLine.concat('   ');
+            midLine.concat('  |');
+            botLine.concat('  |');
+            break;
+        case "2":
+            topLine.concat(' _ ');
+            midLine.concat(' _|');
+            botLine.concat('|_');
+            break;
+        case "3":
+            topLine.concat(' _ ');
+            midLine.concat(' _|');
+            botLine.concat(' _|');
+            break;
+        case "4":
+            topLine.concat('   ');
+            midLine.concat('|_|');
+            botLine.concat('  |');
+            break;
+        case "5":
+            topLine.concat(' _ ');
+            midLine.concat('|_ ');
+            botLine.concat(' _|');
+            break;
+        case "6":
+            topLine.concat('   ');
+            midLine.concat('|_ ');
+            botLine.concat('|_|');
+            break;
+        case "7":
+            topLine.concat(' _ ');
+            midLine.concat('  |');
+            botLine.concat('  |');
+            break;
+        case "8":
+            topLine.concat(' _ ');
+            midLine.concat('|_|');
+            botLine.concat('|_|');
+            break;
+        case "9":
+            topLine.concat(' _ ');
+            midLine.concat('|_|');
+            botLine.concat('  |');
+            break;
+    };
+};
